9d392b99bd8e1b402b38c59ef79ae213381b7185
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-06-05 11:45:03 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,9 +31,16 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/login" do
-		user = User.new(:username => params[:username], :password => params[:password])
+    user = User.find_by(:username => params[:username])
+ 
+    if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect "/success"
+    else
+        redirect "/failure"
+    end
 	end
-
+	
 	get "/success" do
 		if logged_in?
 			erb :success
